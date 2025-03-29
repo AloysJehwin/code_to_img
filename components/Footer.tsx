7079a2a036d07945be0e775373fd67484242a994
AloysJehwin <aloysjehwin@gmail.com>
2025-03-29 17:06:32 +0530

    Final Update

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="flex items-center gap-10 py-16">
+      <Link
+        href="https://github.com/AloysJehwin/code_to_img"
+        className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
+      >
+        Source Code Here &nbsp;&#129109;
+      </Link>
+    </div>
+  );
+}
+
+export default Footer;
